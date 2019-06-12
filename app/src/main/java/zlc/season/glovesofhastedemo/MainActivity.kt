2046ad509c0f7f85aa7765b8e175f9c38e79c6d7
ssseasonnn <ssseasonnn@gmail.com>
2019-06-12 18:08:47 +0800

    add jitpack plugin

--- app/src/main/java/zlc/season/glovesofhastedemo/MainActivity.kt
+++ app/src/main/java/zlc/season/glovesofhastedemo/MainActivity.kt
@@ -2,13 +2,12 @@ package zlc.season.glovesofhastedemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import zlc.season.glovesofhaste.logd
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        "".logd()
+        
     }
 }
