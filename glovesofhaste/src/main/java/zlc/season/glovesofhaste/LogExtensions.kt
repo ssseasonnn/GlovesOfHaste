3ef68640f8971278579b81bac6737dea92a8e0b7
ssseasonnn <ssseasonnn@gmail.com>
2019-06-12 10:27:55 +0800

    add jit pack plugin

---
+++ glovesofhaste/src/main/java/zlc/season/glovesofhaste/LogExtensions.kt
@@ -0,0 +1,55 @@
+package zlc.season.glovesofhaste
+
+import android.util.Log.*
+
+private var DEBUG = true
+private var LOG_TAG = "TAG"
+
+fun setLogTag(tag: String) {
+    LOG_TAG = tag
+}
+
+fun setDebug(flag: Boolean) {
+    DEBUG = flag
+}
+
+fun <T> T.logd(tag: String = ""): T {
+    innerLog(::d, ::d, tag)
+    return this
+}
+
+
+fun <T> T.logi(tag: String = ""): T {
+    innerLog(::i, ::i, tag)
+    return this
+}
+
+fun <T> T.logw(tag: String = ""): T {
+    innerLog(::w, ::w, tag)
+    return this
+}
+
+fun <T> T.loge(tag: String = ""): T {
+    innerLog(::e, ::e, tag)
+    return this
+}
+
+fun <T> T.logv(tag: String = ""): T {
+    innerLog(::v, ::v, tag)
+    return this
+}
+
+private fun <T> T.innerLog(
+    function1: (String, String, Throwable) -> Int,
+    function2: (String, String?) -> Int,
+    tag: String
+) {
+    if (DEBUG) {
+        val realTag = if (tag.isNotEmpty()) tag else LOG_TAG
+        if (this is Throwable) {
+            function1(realTag, this.message ?: "", this)
+        } else {
+            function2(realTag, this.toString())
+        }
+    }
+}
