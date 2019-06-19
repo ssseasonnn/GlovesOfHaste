de4be8426b183de324dd87bd54dea3174431f30f
ssseasonnn <ssseasonnn@gmail.com>
2019-06-19 18:08:58 +0800

    toast tricks

--- app/src/main/java/zlc/season/glovesofhastedemo/MainActivity.kt
+++ app/src/main/java/zlc/season/glovesofhastedemo/MainActivity.kt
@@ -2,6 +2,7 @@ package zlc.season.glovesofhastedemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import zlc.season.glovesofhaste.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val a = "hhh".toast()
     }
 }
